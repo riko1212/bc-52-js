b9702bbd5667003de6f411925363ef6afd73da5e
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-04-07 09:18:52 +0300

    +++

---
+++ js/for-each.js
@@ -0,0 +1,48 @@
+/*
+ * forEach() замість циклів for і for...of для масивів
+ */
+
+const fruits = ['🍎', '🍇', '🍑', '🍌', '🍋'];
+
+/*
+? Виконайте рефакторинг коду, використовуючи метод forEach і стрілочні функції.
+*/
+// function logItems(items) {
+//   for (let i = 0; i < items.length; i += 1) {
+//     console.log(`${i + 1} - ${items[i]}`);
+//   }
+
+// logItems(['🍎', '🍇', '🍑', '🍌', '🍋']);
+
+/*
+? Виконайте рефакторинг коду, використовуючи метод forEach і стрілочні функції.
+*/
+// function printContactsInfo({ names, phones } = {}) {
+//   const nameList = names.split(',');
+//   const phoneList = phones.split(',');
+
+// for (let i = 0; i < nameList.length; i += 1) {
+//   console.log(`${nameList[i]}: ${phoneList[i]}`);
+// }
+
+// printContactsInfo({
+//   names: 'Jacob,William,Solomon,Artemis',
+//   phones: '89001234567,89001112233,890055566377,890055566300',
+// });
+
+/*
+? Виконайте рефакторинг коду, використовуючи метод forEach і стрілочні функції.
+*/
+// const calсulateAverage = function(...args) {
+//   let total = 0;
+
+// for (let i = 0; i < args.length; i += 1) {
+//   total += args[i];
+// }
+
+//   return total / args.length;
+// };
+
+// console.log(calсulateAverage(1, 2, 3, 4)); // 2.5
+// console.log(calсulateAverage(14, 8, 2)); // 8
+// console.log(calсulateAverage(27, 43, 2, 8, 36)); // 23.2/
