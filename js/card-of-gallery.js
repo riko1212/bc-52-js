d9fd7950a13154150ba6c797f54f0fc46149bc7c
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-04-17 08:33:13 +0300

    +++

---
+++ js/card-of-gallery.js
@@ -0,0 +1,15 @@
+/*
+<li class="gallery-item">
+  <a href="#">
+    <img src="https://picsum.photos/id/237/200/300" alt="Labrador">
+  </a>
+</li>
+*/
+
+// Створення li
+
+// Створення a
+
+// Створення img
+
+// Вставка елементів
---
+++ js/create-gallery.js
@@ -0,0 +1,52 @@
+const pictures = [
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/0/700/460',
+    alt: 'laptop',
+  },
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/1/700/460',
+    alt: 'developer',
+  },
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/10/700/460',
+    alt: 'forest',
+  },
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/100/700/460',
+    alt: 'beach',
+  },
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/1000/700/460',
+    alt: 'mountain',
+  },
+];
+
+/*
+<li class="gallery-item">
+  <a href="#">
+    <img src="https://picsum.photos/id/237/200/300" alt="Labrador" width="" height="">
+  </a>
+</li>
+*/
+
+// Функція для створення карточки makeGalleryCard(cardInfo)
+
+// Створення li
+
+// Створення a
+
+// Створення img
+
+// Перебір масиву pictures через map
+
+// Вставка колекції карток на сторінку
