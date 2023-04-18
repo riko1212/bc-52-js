4038ffe626465b82412ab30290b6034b6b11a859
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-04-18 12:55:59 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -13,18 +13,29 @@ const toggleModal = () => {
   backdropEl.classList.toggle('is-open');
 };
 
-const onEscBtnClick = (e) => {
-  if (e.code === 'Escape' && backdropEl.classList.contains('is-open')) {
-    console.log(e.code);
-    toggleModal();
-    document.removeEventListener('keydown', onEscBtnClick);
-  }
-};
+// const onEscBtnClick = (e) => {
+//   if (e.code === 'Escape' && backdropEl.classList.contains('is-open')) {
+//     console.log(e.code);
+//     toggleModal();
+//     // document.removeEventListener('keydown', onEscBtnClick);
+//   }
+// };
 
 const onModalOpenBtnClick = (e) => {
   toggleModal();
-  document.addEventListener('keydown', onEscBtnClick);
+  console.log(e.code);
+  document.addEventListener('keydown', toggleModal, {
+    once: true,
+  });
+};
+
+const onBackdropClick = (e) => {
+  // if (!e.target.classList.contains('js-backdrop')) {
+  if (e.target === e.currentTarget) {
+    toggleModal();
+  }
 };
 
 modalOpenBtnEl.addEventListener('click', onModalOpenBtnClick);
 modalCloseBtnEl.addEventListener('click', toggleModal);
+backdropEl.addEventListener('click', onBackdropClick);
