7fa35cd3fc0be94a10944bf3f413712c7597b1b3
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-04-26 13:02:26 +0300

    q&a

--- src/js/heroes.js
+++ src/js/heroes.js
@@ -1,12 +1,43 @@
-/*
-<li class="heroes__item">
-    <img src="${url}" alt="${name}" class="heroes__picture">
-    <div class="heroes__content">
-        <h2 class="heroes__name">${name}</h2>
-        <ul class="heroes__biography list">
-            <li class="heroes__biography-item"><p class="heroes__gender"><strong>Gender: </strong> ${gender}</p></li>
-            <li class="heroes__biography-item"><p class="heroes__alter-egos"><strong>Alter Egos: </strong> ${alterEgos}</p></li>
-        </ul>
-    </div>
-</li>
-*/
+import heroesDb from '../json/heroes.json';
+import createHeroeCard from '../templates/heroes.hbs';
+
+const heroesListEl = document.querySelector('.js-heroes-list');
+
+const heroesArr = createHeroeCard(heroesDb);
+
+heroesListEl.innerHTML = heroesArr;
+
+// import Handlebars from 'handlebars';
+
+// const template = Handlebars.compile('<p>Name: {{age}}</p>');
+
+// console.log(template({ age: 40 }));
+
+// const createHeroesCard = heroes => {
+//   let {
+//     images: { lg: url },
+//     name,
+//     appearance: { gender },
+//     biography: { alterEgos },
+//   } = heroes;
+//   if (alterEgos === null) {
+//     alterEgos = '-';
+//   }
+//   return `
+//     <li class="heroes__item">
+//     <img src="${url}" alt="${name}" class="heroes__picture">
+//     <div class="heroes__content">
+//         <h2 class="heroes__name">${name}</h2>
+//         <ul class="heroes__biography list">
+//             <li class="heroes__biography-item"><p class="heroes__gender"><strong>Gender: </strong> ${gender}</p></li>
+//             <li class="heroes__biography-item"><p class="heroes__alter-egos"><strong>Alter Egos: </strong> ${alterEgos}</p></li>
+//         </ul>
+//     </div>
+// </li>`;
+// };
+
+// const heroesArr = heroesDb.map(el => {
+//   return createHeroeCard(el);
+// });
+
+// heroesListEl.innerHTML = heroesArr.join('');
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,49 @@
 // http://latentflip.com/loupe/?code=Y29uc29sZS5sb2coJ2ZpcnN0Jyk7CgpzZXRUaW1lb3V0KGZ1bmN0aW9uKCkgewogICAgY29uc29sZS5sb2coJ2lubmVyIHNldFRpbWVvdXQnKTsKfSwgMTAwMCk7Cgpjb25zb2xlLmxvZygnc2Vjb25kJyk7!!!
 
+// console.log('start');
 
+// setTimeout(() => {
+//   console.log('SetTimeout');
+// }, 100);
+
+// const promise = new Promise((resolve, reject) => {
+//   console.log('In constructor Promise');
+
+//   resolve('In then');
+// });
+
+// promise.then(data => {
+//   setTimeout(() => {
+//     console.log('timeout data', data);
+//   }, 0);
+//   console.log(data);
+// });
+
+// console.log('end');
+
+const containerEl = document.querySelector('.js-counter');
+
+// for (let i = 0; i <= 1000; i += 1) {
+//   containerEl.innerHTML = i;
+// }
+
+let i = 0;
+
+// const count = () => {
+//   i += 10;
+//   containerEl.innerHTML = i;
+//   if (i != 1000) {
+//     setTimeout(() => {
+//       count();
+//     }, 0);
+//   }
+// };
+
+// const count = () => {
+//   i += 10;
+//   containerEl.innerHTML = i;
+
+//   count();
+// };
+
+count();
