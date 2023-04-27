e318d3dac507da3f103ab375d791fad14087b342
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-04-27 09:35:32 +0300

    Update weather.js

--- src/js/weather.js
+++ src/js/weather.js
@@ -11,31 +11,3 @@ const weatherWrapperEl = document.querySelector('.js-weather__wrapper');
 //     0
 //   )}`;
 // };
-
-const onWeatherFormSubmit = event => {
-  event.preventDefault();
-
-  const weatherQuery = event.currentTarget.elements.user_country.value;
-
-  fetchCurrentWeather(weatherQuery)
-    .then(data => {
-      console.log(data);
-
-      data.sys.sunrise = convertSecondsToHoursAndMinutes(data.sys.sunrise);
-      data.sys.sunset = convertSecondsToHoursAndMinutes(data.sys.sunset);
-
-      weatherWrapperEl.innerHTML = createWeatherCard(data);
-    })
-    .catch(err => {
-      if (err.message === '404') {
-        alert('Такого міста не знайдено');
-
-        weatherWrapperEl.innerHTML = '';
-      }
-    })
-    .finally(() => {
-      event.target.reset();
-    });
-};
-
-weatherFormEl.addEventListener('submit', onWeatherFormSubmit);
